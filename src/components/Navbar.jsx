06c329d66374433abdfc00f3730672d142e29b0b
alain17-web <web2020.alain@gmail.com>
2023-08-12 23:11:01 +0200

    Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,44 +7,61 @@ const Section = styled.div`
 
 const Container = styled.div`
     width: 1400px;
-    background-color: rebeccapurple;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 10px 0;
 `;
 
 const Links = styled.div`
-
+    display: flex;
+    align-items: center;
+    gap: 50px;
 `;
 
 const Logo = styled.img`
-
+    height: 50px;
 `;
 
 const List = styled.ul`
-
+    display: flex;
+    gap: 20px;
+    list-style: none;
 `;
 
 const ListItem = styled.li`
-
+    cursor: pointer;
 `;
 
 const Icons = styled.div`
-
+    display: flex;
+    align-items: center;
+    gap: 20px;
 `;
-const Icon = styled.img`
 
+const Icon = styled.img`
+    width: 20px;
+    cursor: pointer;
 `;
 
 const Button = styled.button`
-
+    width: 100px;
+    padding: 10px;
+    background-color: #da4ea2;
+    color: white;
+    border: none;
+    cursor: pointer;
+    border-radius: 5px;
 `;
 
 
 
 const Navbar = () => {
-    return ( 
+    return (
         <Section>
             <Container>
                 <Links>
-                    <Logo src="./img/logo.png" alt="logo"/>
+                    <Logo src="./img/logo.png" alt="logo" />
                     <List>
                         <ListItem>Home</ListItem>
                         <ListItem>Studio</ListItem>
@@ -53,12 +70,12 @@ const Navbar = () => {
                     </List>
                 </Links>
                 <Icons>
-                    <Icon src="./img/search.png"/>
+                    <Icon src="./img/search.png" />
                     <Button>Hire now</Button>
                 </Icons>
             </Container>
         </Section>
-     );
+    );
 }
- 
+
 export default Navbar;
